3b67cf092f3b4750d51675b1f364bab6433da169
jochen <jochen@byte23.net>
2024-10-22 16:55:57 +0200

    added component Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@ export default function Card() {
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl">
       <figure>
-        <img src={image} alt={alt - text} />
+        <img src={image} alt={imgtext} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Open</button>
+          <button className="btn btn-primary">View Detail</button>
         </div>
       </div>
     </div>
