7590ed56d810aa5fd486976f68b80aad254a18c9
jochen <jochen@byte23.net>
2024-10-22 17:27:12 +0200

    corrected an error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
-export default function Card() {
-  return (
-    <div className="card lg:card-side bg-base-100 shadow-xl">
-      <figure>
-        <img src={image} alt={imgtext} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">View Detail</button>
+import ButtonInfo from './ButtonInfo';
+
+export default function Card({ image, imgtext, title }) {
+    return (
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+            <figure>
+                <img src={image} alt={imgtext} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <div className="card-actions justify-end">
+                    <ButtonInfo />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
